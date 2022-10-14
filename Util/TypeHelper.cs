ae58556701056b46e33b25ecac88b5bf5333d902
Yi Uta <UtaYiZzzz@outlook.com>
2022-10-14 14:34:35 +0800

    feat: 类型帮助类中加入SwicthType方法, 用于相对优雅得执行较长的类型分支判断

--- Util/TypeHelper.cs
+++ Util/TypeHelper.cs
@@ -38,5 +38,168 @@ namespace Util
             }
             return result;
         }
+
+
+        #region 类型分支判断
+        /// <summary>
+        /// 对不同分支类型, 执行不同操作的构建类
+        /// <para>输入对象为null时, 使用类型模式 : 只判断类型, 无需将对象作为输入</para>
+        /// <para>输入对象不为null时, 使用对象模式 : 判断输入对象的类型, 需要将对象作为输入</para>
+        /// </summary>
+        public class TypeSwitchBuilder
+        {
+            #region 输入对象
+            private Type InputType { get; set; }
+            private object InputObj { get; set; }
+
+            #endregion
+            public TypeSwitchBuilder(object inputObj)
+            {
+                InputObj = inputObj;
+                InputType = inputObj?.GetType();
+            }
+            public TypeSwitchBuilder(Type inputType)
+            {
+                InputType = inputType;
+                InputObj = null;
+            }
+            #region 构成
+
+
+            #region 构成内容
+            /// <summary>
+            /// 类型模式的类型与Action对应字典
+            /// </summary>
+            private Dictionary<Type, Action> TypeModeActions = new Dictionary<Type, Action>();
+            /// <summary>
+            /// 对象模式的类型与Action对应字典
+            /// </summary>
+            private Dictionary<Type, Action<object>> ObjModeActions = new Dictionary<Type, Action<object>>();
+            /// <summary>
+            /// 默认情况下的执行方法 (类型未匹配与输入的类型匹配)
+            /// </summary>
+            private Action DefaultAction;
+            #endregion
+            /// <summary>
+            /// 输入类型的情况下执行
+            /// </summary>
+            /// <typeparam name="T"></typeparam>
+            /// <param name="doSomething"></param>
+            /// <returns></returns>
+            public TypeSwitchBuilder Case<T>(Action<T> doSomething)
+            {
+                if (doSomething == null) throw new ArgumentNullException(nameof(doSomething));
+                Type type = typeof(T);
+                if (!ObjModeActions.ContainsKey(type))
+                {
+                    ObjModeActions.Add(type, (obj) => doSomething.Invoke((T)obj));
+                }
+                else
+                {
+                    throw new ArgumentException($"输入的类型重复: {type.Name}", nameof(T));
+                }
+                return this;
+            }
+            /// <summary>
+            /// 输入类型的情况下执行
+            /// </summary>
+            /// <param name="doSomething"></param>
+            /// <returns></returns>
+            public TypeSwitchBuilder Case<T>(Action doSomething)
+            {
+                if (doSomething == null) throw new ArgumentNullException(nameof(doSomething));
+                Type type = typeof(T);
+                if (!TypeModeActions.ContainsKey(type))
+                {
+                    TypeModeActions.Add(type, doSomething);
+                }
+                else
+                {
+                    throw new ArgumentException($"输入的类型重复: {type.Name}", nameof(T));
+                }
+                return this;
+            }
+            /// <summary>
+            /// 默认情况下执行
+            /// </summary>
+            /// <param name="doSomething"></param>
+            /// <returns></returns>
+            public TypeSwitchBuilder Default(Action doSomething)
+            {
+                DefaultAction = doSomething;
+                return this;
+            }
+            #endregion
+            /// <summary>
+            /// 执行
+            /// </summary>
+            public void Run()
+            {
+                if (InputType == null) throw new ArgumentNullException(nameof(InputType));
+
+                if (InputObj == null)
+                {
+                    RunTypeMode();
+                }
+                else
+                {
+                    RunObjMode();
+                }
+            }
+            private void RunTypeMode()
+            {
+                foreach (Type type in TypeModeActions.Keys)
+                {
+                    if (InputType.Equals(type))
+                    {
+                        TypeModeActions[type].Invoke();
+                        return;
+                    }
+                }
+                DefaultAction.Invoke();
+            }
+            private void RunObjMode()
+            {
+                foreach (Type type in ObjModeActions.Keys)
+                {
+                    if (InputType.Equals(type))
+                    {
+                        ObjModeActions[type].Invoke(InputObj);
+                        return;
+                    }
+                }
+                DefaultAction.Invoke();
+            }
+        }
+        /// <summary>
+        /// 取得对不同分支类型, 执行不同操作的构建类实例
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static TypeSwitchBuilder SwitchType(object obj)
+        {
+            return new TypeSwitchBuilder(obj);
+        }
+        /// <summary>
+        /// 取得对不同分支类型, 执行不同操作的构建类实例
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static TypeSwitchBuilder SwitchType<T>()
+        {
+            return new TypeSwitchBuilder(typeof(T));
+        }
+        /// <summary>
+        /// 取得对不同分支类型, 执行不同操作的构建类实例
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static TypeSwitchBuilder SwitchType(Type type)
+        {
+            return new TypeSwitchBuilder(type);
+        }
+        #endregion
+
+
     }
 }
---
+++ WinFormsTest/Tests/Util/SwitchTypeTest001.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsTest.Tests
+{
+    public partial class SwitchTypeTest001 : TestFormBase
+    {
+        public SwitchTypeTest001()
+        {
+            InitializeComponent();
+        }
+
+        public override void TestContent()
+        {
+            base.TestContent();
+
+            List<Type> waitCheckType = new List<Type>()
+            {
+                typeof(string), typeof(SwitchTypeTest001), typeof(int), typeof(short), typeof(内容)
+            };
+            List<object> waitCheckObj = new List<object>()
+            {
+                "111", 222, 33u, 44L, 0x555, true, this, 内容.对象模式, new { index = 1 }
+            };
+
+            Log(内容.类型模式, "开始");
+            foreach (Type type in waitCheckType)
+            {
+                Util.TypeHelper.SwitchType(type)
+                    .Case<int>(() =>
+                    {
+                        Log(内容.类型模式,  "输入: " + type.FullName + " 执行过程: " + typeof(int).FullName);
+                    })
+                    .Case<string>(() =>
+                    {
+                        Log(内容.类型模式, "输入: " + type.FullName + " 执行过程: " + typeof(string).FullName);
+                    })
+                    .Case<bool>(() =>
+                    {
+                        Log(内容.类型模式, "输入: " + type.FullName + " 执行过程: " + typeof(bool).FullName);
+                    })
+                    .Case<short>(() =>
+                    {
+                        Log(内容.类型模式, "输入: " + type.FullName + " 执行过程: " + typeof(short).FullName);
+                    })
+                    .Case<内容>(() =>
+                    {
+                        Log(内容.类型模式, "输入: " + type.FullName + " 执行过程: " + typeof(内容).FullName);
+                    })
+                    .Default(() =>
+                    {
+                        Log(内容.类型模式, "默认调用");
+                    })
+                    .Run();
+            }
+            Log(内容.对象模式, "开始");
+            foreach (object forObj in waitCheckObj)
+            {
+                Util.TypeHelper.SwitchType(forObj)
+                    .Case<int>((obj) =>
+                    {
+                        Log(内容.类型模式, "输入: " + forObj.GetType().FullName + " 执行过程: " + obj.GetType().FullName + ": " + obj.ToString());
+                    })
+                    .Case<string>((obj) =>
+                    {
+                        Log(内容.类型模式, "输入: " + forObj.GetType().FullName + " 执行过程: " + obj.GetType().FullName + ": " + obj.ToString());
+                    })
+                    .Case<bool>((obj) =>
+                    {
+                        Log(内容.类型模式, "输入: " + forObj.GetType().FullName + " 执行过程: " + obj.GetType().FullName + ": " + obj.ToString());
+                    })
+                    .Case<short>((obj) =>
+                    {
+                        Log(内容.类型模式, "输入: " + forObj.GetType().FullName + " 执行过程: " + obj.GetType().FullName + ": " + obj.ToString());
+                    })
+                    .Case<内容>((obj) =>
+                    {
+                        Log(内容.类型模式, "输入: " + forObj.GetType().FullName + " 执行过程: " + obj.GetType().FullName + ": " + obj.ToString());
+                    })
+                    .Default(() =>
+                    {
+                        Log(内容.类型模式, "默认调用: " + forObj.GetType().FullName + " 值: "+ forObj.ToString());
+                    })
+                    .Run();
+            }
+        }
+        public enum 内容
+        {
+            类型模式,
+            对象模式
+        }
+    }
+}
