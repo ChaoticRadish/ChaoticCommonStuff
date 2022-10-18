06d71b4cf8aac8bba4eadf8b9cf515a198799a2e
Yi Uta <UtaYiZzzz@outlook.com>
2022-10-18 16:52:32 +0800

    feat: 文件选择框加入是否必须存在文件的配置项

--- ChaoticWinformControl/TextBox/FileSelectTextBox.cs
+++ ChaoticWinformControl/TextBox/FileSelectTextBox.cs
@@ -27,6 +27,11 @@ namespace ChaoticWinformControl
         /// </summary>
         [Category("自定义属性"), Description("文件过滤字符串")]
         public string SelectFormTitle { get; set; }
+        /// <summary>
+        /// 文件是否必须存在
+        /// </summary>
+        [Category("自定义属性"), Description("文件是否必须存在")]
+        public bool FileMustExist { get; set; } = false;
 
         /// <summary>
         /// 当前选择的文件名就是 <see cref="TextBox.Text"/>
@@ -76,13 +81,28 @@ namespace ChaoticWinformControl
 
         protected override void OnClick(EventArgs e)
         {
-            SaveFileDialog form = new SaveFileDialog()
+            FileDialog form;
+            if (FileMustExist)
             {
-                Filter = Filter,
-                Title = SelectFormTitle,
-                ValidateNames = true,
-                RestoreDirectory = true,
-            };
+                form = new OpenFileDialog()
+                {
+                    Filter = Filter,
+                    Title = SelectFormTitle,
+                    Multiselect = false,
+                    ValidateNames = true,
+                    RestoreDirectory = true,
+                };
+            }
+            else
+            {
+                form = new SaveFileDialog()
+                {
+                    Filter = Filter,
+                    Title = SelectFormTitle,
+                    ValidateNames = true,
+                    RestoreDirectory = true,
+                };
+            }
             if (form.ShowDialog(FindForm()) == DialogResult.OK)
             {
                 string fileName = form.FileName;
