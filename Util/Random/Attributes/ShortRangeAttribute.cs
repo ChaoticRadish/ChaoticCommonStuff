b9c1c71e39979cfc48513dccce794d7a3b5c97fc
Yi Uta <UtaYiZzzz@outlook.com>
2022-10-25 08:31:16 +0800

    feat: 随机对象帮助类增加short类型支持

---
+++ Util/Random/Attributes/ShortRangeAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.Random
+{
+    /// <summary>
+    /// short类型区间, 可用来表示长度范围
+    /// </summary>
+    public class ShortRangeAttribute : Attribute
+    {
+        public ShortRangeAttribute(short min, short max)
+        {
+            if (max < min)
+            {
+                short temp = min;
+                min = max;
+                max = temp;
+            }
+            Min = min;
+            Max = max;
+        }
+        /// <summary>
+        /// 范围设置为 (0, maxlength)
+        /// </summary>
+        /// <param name="maxlength"></param>
+        public ShortRangeAttribute(short maxlength) : this(0, maxlength) { }
+
+        public short Min { get; private set; }
+        public short Max { get; private set; }
+    }
+}
